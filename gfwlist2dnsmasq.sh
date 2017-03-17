97dd2dc5fff9163a4aabfce64d13373f82664eef
Jimmy Situ <web@jimmystone.cn>
2017-03-17 23:48:43 +0800

    Fix typo, --d to -d

--- gfwlist2dnsmasq.sh
+++ gfwlist2dnsmasq.sh
@@ -162,7 +162,7 @@ process(){
 		printf '\033[31m\nFailed to fetch gfwlist.txt. Please check your Internet connection.\033[m\n'
 		clean_and_exit 2
 	fi
-	base64 --d $BASE64_FILE > $GFWLIST_FILE || ( printf '\033[31mFailed to decode gfwlist.txt. Quit.\033[m\n'; clean_and_exit 2 )
+	base64 -d $BASE64_FILE > $GFWLIST_FILE || ( printf '\033[31mFailed to decode gfwlist.txt. Quit.\033[m\n'; clean_and_exit 2 )
 	printf ' Done.\n\n'
 
 	# Convert
