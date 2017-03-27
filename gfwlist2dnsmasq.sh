cf041131ddb235446f028a8ccdf0198e8e0ff6fe
billsq <billsq@billsq.me>
2017-03-27 00:35:45 -0700

    Add an argument to provide a file which contains extra domains.

--- gfwlist2dnsmasq.sh
+++ gfwlist2dnsmasq.sh
@@ -62,6 +62,7 @@ get_args(){
 	FILE_FULLPATH=''
 	CURL_EXTARG=''
 	WITH_IPSET=0
+	EXTRA_DOMAIN_FILE=''
 
 	while [ ${#} -gt 0 ]; do
 		case "${1}" in
@@ -90,6 +91,10 @@ get_args(){
 				OUT_FILE="$2"
 				shift
 				;;
+			--extra | -e)
+				EXTRA_DOMAIN_FILE="$2"
+				shift
+				;;
 			*)
 				echo "Invalid argument: $1"
 				usage 1
@@ -113,7 +118,7 @@ get_args(){
 			fi
 		fi
 	fi
-	
+
 	if [ $OUT_TYPE = 'DNSMASQ_RULES' ]; then
 		# Check DNS IP
 		IP_TEST=$(echo $DNS_IP | grep -E '^((2[0-4][0-9]|25[0-5]|[01]?[0-9][0-9]?)\.){3}(2[0-4][0-9]|25[0-5]|[01]?[0-9][0-9]?)$')
@@ -140,6 +145,11 @@ get_args(){
 				WITH_IPSET=1
 			fi
 		fi
+	fi
+
+	if [ ! -z $EXTRA_DOMAIN_FILE ] && [ ! -f $EXTRA_DOMAIN_FILE ]; then
+	    printf '\033[33m\nWARNING:\nExtra domain file does not exist, ignored.\033[m\n\n'
+	    EXTRA_DOMAIN_FILE=''
 	fi
 }
 
@@ -180,7 +190,7 @@ process(){
 
 	printf 'google.com\ngoogle.ad\ngoogle.ae\ngoogle.com.af\ngoogle.com.ag\ngoogle.com.ai\ngoogle.al\ngoogle.am\ngoogle.co.ao\ngoogle.com.ar\ngoogle.as\ngoogle.at\ngoogle.com.au\ngoogle.az\ngoogle.ba\ngoogle.com.bd\ngoogle.be\ngoogle.bf\ngoogle.bg\ngoogle.com.bh\ngoogle.bi\ngoogle.bj\ngoogle.com.bn\ngoogle.com.bo\ngoogle.com.br\ngoogle.bs\ngoogle.bt\ngoogle.co.bw\ngoogle.by\ngoogle.com.bz\ngoogle.ca\ngoogle.cd\ngoogle.cf\ngoogle.cg\ngoogle.ch\ngoogle.ci\ngoogle.co.ck\ngoogle.cl\ngoogle.cm\ngoogle.cn\ngoogle.com.co\ngoogle.co.cr\ngoogle.com.cu\ngoogle.cv\ngoogle.com.cy\ngoogle.cz\ngoogle.de\ngoogle.dj\ngoogle.dk\ngoogle.dm\ngoogle.com.do\ngoogle.dz\ngoogle.com.ec\ngoogle.ee\ngoogle.com.eg\ngoogle.es\ngoogle.com.et\ngoogle.fi\ngoogle.com.fj\ngoogle.fm\ngoogle.fr\ngoogle.ga\ngoogle.ge\ngoogle.gg\ngoogle.com.gh\ngoogle.com.gi\ngoogle.gl\ngoogle.gm\ngoogle.gp\ngoogle.gr\ngoogle.com.gt\ngoogle.gy\ngoogle.com.hk\ngoogle.hn\ngoogle.hr\ngoogle.ht\ngoogle.hu\ngoogle.co.id\ngoogle.ie\ngoogle.co.il\ngoogle.im\ngoogle.co.in\ngoogle.iq\ngoogle.is\ngoogle.it\ngoogle.je\ngoogle.com.jm\ngoogle.jo\ngoogle.co.jp\ngoogle.co.ke\ngoogle.com.kh\ngoogle.ki\ngoogle.kg\ngoogle.co.kr\ngoogle.com.kw\ngoogle.kz\ngoogle.la\ngoogle.com.lb\ngoogle.li\ngoogle.lk\ngoogle.co.ls\ngoogle.lt\ngoogle.lu\ngoogle.lv\ngoogle.com.ly\ngoogle.co.ma\ngoogle.md\ngoogle.me\ngoogle.mg\ngoogle.mk\ngoogle.ml\ngoogle.com.mm\ngoogle.mn\ngoogle.ms\ngoogle.com.mt\ngoogle.mu\ngoogle.mv\ngoogle.mw\ngoogle.com.mx\ngoogle.com.my\ngoogle.co.mz\ngoogle.com.na\ngoogle.com.nf\ngoogle.com.ng\ngoogle.com.ni\ngoogle.ne\ngoogle.nl\ngoogle.no\ngoogle.com.np\ngoogle.nr\ngoogle.nu\ngoogle.co.nz\ngoogle.com.om\ngoogle.com.pa\ngoogle.com.pe\ngoogle.com.pg\ngoogle.com.ph\ngoogle.com.pk\ngoogle.pl\ngoogle.pn\ngoogle.com.pr\ngoogle.ps\ngoogle.pt\ngoogle.com.py\ngoogle.com.qa\ngoogle.ro\ngoogle.ru\ngoogle.rw\ngoogle.com.sa\ngoogle.com.sb\ngoogle.sc\ngoogle.se\ngoogle.com.sg\ngoogle.sh\ngoogle.si\ngoogle.sk\ngoogle.com.sl\ngoogle.sn\ngoogle.so\ngoogle.sm\ngoogle.sr\ngoogle.st\ngoogle.com.sv\ngoogle.td\ngoogle.tg\ngoogle.co.th\ngoogle.com.tj\ngoogle.tk\ngoogle.tl\ngoogle.tm\ngoogle.tn\ngoogle.to\ngoogle.com.tr\ngoogle.tt\ngoogle.com.tw\ngoogle.co.tz\ngoogle.com.ua\ngoogle.co.ug\ngoogle.co.uk\ngoogle.com.uy\ngoogle.co.uz\ngoogle.com.vc\ngoogle.co.ve\ngoogle.vg\ngoogle.co.vi\ngoogle.com.vn\ngoogle.vu\ngoogle.ws\ngoogle.rs\ngoogle.co.za\ngoogle.co.zm\ngoogle.co.zw\ngoogle.cat\n' >> $DOMAIN_FILE
 	echo 'Google search domains... Added.'
-	
+
 	# Add blogspot domains
 	printf 'blogspot.ca\nblogspot.co.uk\nblogspot.com\nblogspot.com.ar\nblogspot.com.au\nblogspot.com.br\nblogspot.com.by\nblogspot.com.co\nblogspot.com.cy\nblogspot.com.ee\nblogspot.com.eg\nblogspot.com.es\nblogspot.com.mt\nblogspot.com.ng\nblogspot.com.tr\nblogspot.com.uy\nblogspot.de\nblogspot.gr\nblogspot.in\nblogspot.mx\nblogspot.ch\nblogspot.fr\nblogspot.ie\nblogspot.it\nblogspot.pt\nblogspot.ro\nblogspot.sg\nblogspot.be\nblogspot.no\nblogspot.se\nblogspot.jp\nblogspot.in\nblogspot.ae\nblogspot.al\nblogspot.am\nblogspot.ba\nblogspot.bg\nblogspot.ch\nblogspot.cl\nblogspot.cz\nblogspot.dk\nblogspot.fi\nblogspot.gr\nblogspot.hk\nblogspot.hr\nblogspot.hu\nblogspot.ie\nblogspot.is\nblogspot.kr\nblogspot.li\nblogspot.lt\nblogspot.lu\nblogspot.md\nblogspot.mk\nblogspot.my\nblogspot.nl\nblogspot.no\nblogspot.pe\nblogspot.qa\nblogspot.ro\nblogspot.ru\nblogspot.se\nblogspot.sg\nblogspot.si\nblogspot.sk\nblogspot.sn\nblogspot.tw\nblogspot.ug\nblogspot.cat\n' >> $DOMAIN_FILE
 	echo 'Blogspot domains... Added.'
@@ -188,7 +198,14 @@ process(){
 	# Add twimg.edgesuit.net
 	printf 'twimg.edgesuit.net\n' >> $DOMAIN_FILE
 	echo 'twimg.edgesuit.net... Added.'
-	
+
+	# Add extra domains
+	if [ ! -z $EXTRA_DOMAIN_FILE ]; then
+	    cat $EXTRA_DOMAIN_FILE >> $DOMAIN_FILE
+	    printf '\n' >> $DOMAIN_FILE
+	fi
+	echo 'Extra domain file '$EXTRA_DOMAIN_FILE'... Added.'
+
 	if [ $OUT_TYPE = 'DNSMASQ_RULES' ]; then
 	# Convert domains into dnsmasq rules
 		if [ $WITH_IPSET -eq 1 ]; then
@@ -209,10 +226,10 @@ ipset=/\1/'$IPSET_NAME'#g' > $CONF_TMP_FILE
 	else
 		sort -u $DOMAIN_FILE > $OUT_TMP_FILE
 	fi
-	
+
 	cp $OUT_TMP_FILE $OUT_FILE
 	printf '\nConverting GfwList to '$OUT_TYPE'... Done.\n\n'
-	
+
 	# Clean up
 	clean_and_exit 0
 }
