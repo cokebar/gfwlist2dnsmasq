0e95e55d4c6f7213e9b475604eae3599d0f44641
cokebar <cokebar@cokebar.info>
2017-11-17 11:27:11 +0100

    Refine Information Output

--- gfwlist2dnsmasq.sh
+++ gfwlist2dnsmasq.sh
@@ -6,6 +6,24 @@
 # Author:      Cokebar Chi
 # Website:     https://github.com/cokebar
 
+_green() {
+    printf '\033[1;31;32m'
+    printf -- "%b" "$1"
+    printf '\033[0m'
+}
+
+_red() {
+    printf '\033[1;31;31m'
+    printf -- "%b" "$1"
+    printf '\033[0m'
+}
+
+_yellow() {
+    printf '\033[1;31;33m'
+    printf -- "%b" "$1"
+    printf '\033[0m'
+}
+
 usage() {
     cat <<-EOF
 
@@ -36,17 +54,17 @@ EOF
 
 clean_and_exit(){
     # Clean up temp files
-    printf 'Cleaning up...'
+    printf 'Cleaning up... '
     rm -rf $TMP_DIR
-    printf ' Done.\n\n'
-    [ $1 -eq 0 ] && printf '\033[32mJob Finished.\033[m\n' || printf '\033[31mExit with Error code '$1'.\033[m\n'
+    _green 'Done\n\n'
+    [ $1 -eq 0 ] && _green 'Job Finished.\n\n' || _red 'Exit with Error code '$1'.\n'
     exit $1
 }
 
 check_depends(){
     which sed base64 curl >/dev/null
     if [ $? != 0 ]; then
-        printf '\033[31mError: Missing Dependency.\nPlease check whether you have the following binaries on you system:\nsed, base64, curl\033[m\n'
+        _red 'Error: Missing Dependency.\nPlease check whether you have the following binaries on you system:\nsed, base64, curl.\n'
         exit 3
     fi
 
@@ -104,7 +122,7 @@ get_args(){
                 shift
                 ;;
             *)
-                echo "Invalid argument: $1"
+                _red "Invalid argument: $1"
                 usage 1
                 ;;
         esac
@@ -113,15 +131,15 @@ get_args(){
 
     # Check path & file name
     if [ -z $OUT_FILE ]; then
-        printf '\033[31mError: Please specify the path to the output file(using -o/--output argument).\033[m\n'
+        _red 'Error: Please specify the path to the output file(using -o/--output argument).\n'
         exit 1
     else
         if [ -z ${OUT_FILE##*/} ]; then
-            printf '\033[31mError: '$OUT_FILE' is a path, not a file.\033[m\n'
+            _red 'Error: '$OUT_FILE' is a path, not a file.\n'
             exit 1
         else
             if [ ${OUT_FILE}a != ${OUT_FILE%/*}a ] && [ ! -d ${OUT_FILE%/*} ]; then
-                printf '\033[31mError: Folder do not exist: '${OUT_FILE%/*}'\033[m\n'
+                _red 'Error: Folder do not exist: '${OUT_FILE%/*}'\n'
                 exit 1
             fi
         fi
@@ -132,13 +150,13 @@ get_args(){
         IPV4_TEST=$(echo $DNS_IP | grep -E $IPV4_PATTERN)
         IPV6_TEST=$(echo $DNS_IP | grep -E $IPV6_PATTERN)
         if [ "$IPV4_TEST" != "$DNS_IP" -a "$IPV6_TEST" != "$DNS_IP" ]; then
-            printf '\033[31mError: Please enter a valid DNS server IP address.\033[m\n'
+            _red 'Error: Please enter a valid DNS server IP address.\n'
             exit 1
         fi
 
         # Check DNS port
         if [ $DNS_PORT -lt 1 -o $DNS_PORT -gt 65535 ]; then
-            printf '\033[31mError: Please enter a valid DNS server port.\033[m\n'
+            _red 'Error: Please enter a valid DNS server port.\n'
             exit 1
         fi
 
@@ -148,7 +166,7 @@ get_args(){
         else
             IPSET_TEST=$(echo $IPSET_NAME | grep -E '^\w+$')
             if [ "$IPSET_TEST" != "$IPSET_NAME" ]; then
-                printf '\033[31mError: Please enter a valid IP set name.\033[m\n'
+                _red 'Error: Please enter a valid IP set name.\n'
                 exit 1
             else
                 WITH_IPSET=1
@@ -157,7 +175,7 @@ get_args(){
     fi
 
     if [ ! -z $EXTRA_DOMAIN_FILE ] && [ ! -f $EXTRA_DOMAIN_FILE ]; then
-        printf '\033[33m\nWARNING:\nExtra domain file does not exist, ignored.\033[m\n\n'
+        _yellow 'WARNING:\nExtra domain file does not exist, ignored.\n\n'
         EXTRA_DOMAIN_FILE=''
     fi
 }
@@ -174,15 +192,14 @@ process(){
     OUT_TMP_FILE="$TMP_DIR/gfwlist.out.tmp"
 
     # Fetch GfwList and decode it into plain text
-    printf '\033[32mJob Started.\n\033[m\n'
-    printf 'Fetching GfwList...'
+    printf 'Fetching GfwList... '
     curl -s -L $CURL_EXTARG -o$BASE64_FILE $BASE_URL
     if [ $? != 0 ]; then
-        printf '\033[31m\nFailed to fetch gfwlist.txt. Please check your Internet connection.\033[m\n'
+        _red '\nFailed to fetch gfwlist.txt. Please check your Internet connection.\n'
         clean_and_exit 2
     fi
-    $BASE64_DECODE $BASE64_FILE > $GFWLIST_FILE || ( printf '\033[31mFailed to decode gfwlist.txt. Quit.\033[m\n'; clean_and_exit 2 )
-    printf ' Done.\n\n'
+    $BASE64_DECODE $BASE64_FILE > $GFWLIST_FILE || ( _red 'Failed to decode gfwlist.txt. Quit.\n'; clean_and_exit 2 )
+    _green 'Done.\n\n'
 
     # Convert
     IGNORE_PATTERN='^\!|\[|^@@|(https?://){0,1}[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+'
@@ -191,37 +208,37 @@ process(){
     DOMAIN_PATTERN='([a-zA-Z0-9][-a-zA-Z0-9]*(\.[a-zA-Z0-9][-a-zA-Z0-9]*)+)'
     HANDLE_WILDCARD_PATTERN='s#^(([a-zA-Z0-9]*\*[-a-zA-Z0-9]*)?(\.))?([a-zA-Z0-9][-a-zA-Z0-9]*(\.[a-zA-Z0-9][-a-zA-Z0-9]*)+)(\*)?#\4#g'
 
-    echo 'Converting GfwList to '$OUT_TYPE'...'
-    printf '\033[33m\nWARNING:\nThe following lines in GfwList contain regex, and might be ignored:\033[m\n\n'
+    printf 'Converting GfwList to ' && _green $OUT_TYPE && printf ' ...\n' 
+    _yellow '\nWARNING:\nThe following lines in GfwList contain regex, and might be ignored:\n\n'
     cat $GFWLIST_FILE | grep -n '^/.*$'
-    printf "\033[33m\nThis script will try to convert some of the regex rules. But you should know this may not be a equivalent conversion.\nIf there's regex rules which this script do not deal with, you should add the domain manually to the list.\033[m\n\n"
+    _yellow "\nThis script will try to convert some of the regex rules. But you should know this may not be a equivalent conversion.\nIf there's regex rules which this script do not deal with, you should add the domain manually to the list.\n\n"
     grep -vE $IGNORE_PATTERN $GFWLIST_FILE | $SED_ERES $HEAD_FILTER_PATTERN | $SED_ERES $TAIL_FILTER_PATTERN | grep -E $DOMAIN_PATTERN | $SED_ERES $HANDLE_WILDCARD_PATTERN > $DOMAIN_FILE
 
     printf 'google.com\ngoogle.ad\ngoogle.ae\ngoogle.com.af\ngoogle.com.ag\ngoogle.com.ai\ngoogle.al\ngoogle.am\ngoogle.co.ao\ngoogle.com.ar\ngoogle.as\ngoogle.at\ngoogle.com.au\ngoogle.az\ngoogle.ba\ngoogle.com.bd\ngoogle.be\ngoogle.bf\ngoogle.bg\ngoogle.com.bh\ngoogle.bi\ngoogle.bj\ngoogle.com.bn\ngoogle.com.bo\ngoogle.com.br\ngoogle.bs\ngoogle.bt\ngoogle.co.bw\ngoogle.by\ngoogle.com.bz\ngoogle.ca\ngoogle.cd\ngoogle.cf\ngoogle.cg\ngoogle.ch\ngoogle.ci\ngoogle.co.ck\ngoogle.cl\ngoogle.cm\ngoogle.cn\ngoogle.com.co\ngoogle.co.cr\ngoogle.com.cu\ngoogle.cv\ngoogle.com.cy\ngoogle.cz\ngoogle.de\ngoogle.dj\ngoogle.dk\ngoogle.dm\ngoogle.com.do\ngoogle.dz\ngoogle.com.ec\ngoogle.ee\ngoogle.com.eg\ngoogle.es\ngoogle.com.et\ngoogle.fi\ngoogle.com.fj\ngoogle.fm\ngoogle.fr\ngoogle.ga\ngoogle.ge\ngoogle.gg\ngoogle.com.gh\ngoogle.com.gi\ngoogle.gl\ngoogle.gm\ngoogle.gp\ngoogle.gr\ngoogle.com.gt\ngoogle.gy\ngoogle.com.hk\ngoogle.hn\ngoogle.hr\ngoogle.ht\ngoogle.hu\ngoogle.co.id\ngoogle.ie\ngoogle.co.il\ngoogle.im\ngoogle.co.in\ngoogle.iq\ngoogle.is\ngoogle.it\ngoogle.je\ngoogle.com.jm\ngoogle.jo\ngoogle.co.jp\ngoogle.co.ke\ngoogle.com.kh\ngoogle.ki\ngoogle.kg\ngoogle.co.kr\ngoogle.com.kw\ngoogle.kz\ngoogle.la\ngoogle.com.lb\ngoogle.li\ngoogle.lk\ngoogle.co.ls\ngoogle.lt\ngoogle.lu\ngoogle.lv\ngoogle.com.ly\ngoogle.co.ma\ngoogle.md\ngoogle.me\ngoogle.mg\ngoogle.mk\ngoogle.ml\ngoogle.com.mm\ngoogle.mn\ngoogle.ms\ngoogle.com.mt\ngoogle.mu\ngoogle.mv\ngoogle.mw\ngoogle.com.mx\ngoogle.com.my\ngoogle.co.mz\ngoogle.com.na\ngoogle.com.nf\ngoogle.com.ng\ngoogle.com.ni\ngoogle.ne\ngoogle.nl\ngoogle.no\ngoogle.com.np\ngoogle.nr\ngoogle.nu\ngoogle.co.nz\ngoogle.com.om\ngoogle.com.pa\ngoogle.com.pe\ngoogle.com.pg\ngoogle.com.ph\ngoogle.com.pk\ngoogle.pl\ngoogle.pn\ngoogle.com.pr\ngoogle.ps\ngoogle.pt\ngoogle.com.py\ngoogle.com.qa\ngoogle.ro\ngoogle.ru\ngoogle.rw\ngoogle.com.sa\ngoogle.com.sb\ngoogle.sc\ngoogle.se\ngoogle.com.sg\ngoogle.sh\ngoogle.si\ngoogle.sk\ngoogle.com.sl\ngoogle.sn\ngoogle.so\ngoogle.sm\ngoogle.sr\ngoogle.st\ngoogle.com.sv\ngoogle.td\ngoogle.tg\ngoogle.co.th\ngoogle.com.tj\ngoogle.tk\ngoogle.tl\ngoogle.tm\ngoogle.tn\ngoogle.to\ngoogle.com.tr\ngoogle.tt\ngoogle.com.tw\ngoogle.co.tz\ngoogle.com.ua\ngoogle.co.ug\ngoogle.co.uk\ngoogle.com.uy\ngoogle.co.uz\ngoogle.com.vc\ngoogle.co.ve\ngoogle.vg\ngoogle.co.vi\ngoogle.com.vn\ngoogle.vu\ngoogle.ws\ngoogle.rs\ngoogle.co.za\ngoogle.co.zm\ngoogle.co.zw\ngoogle.cat\n' >> $DOMAIN_FILE
-    echo 'Google search domains... Added.'
+    printf 'Google search domains... ' && _green 'Added\n'
 
     # Add blogspot domains
     printf 'blogspot.ca\nblogspot.co.uk\nblogspot.com\nblogspot.com.ar\nblogspot.com.au\nblogspot.com.br\nblogspot.com.by\nblogspot.com.co\nblogspot.com.cy\nblogspot.com.ee\nblogspot.com.eg\nblogspot.com.es\nblogspot.com.mt\nblogspot.com.ng\nblogspot.com.tr\nblogspot.com.uy\nblogspot.de\nblogspot.gr\nblogspot.in\nblogspot.mx\nblogspot.ch\nblogspot.fr\nblogspot.ie\nblogspot.it\nblogspot.pt\nblogspot.ro\nblogspot.sg\nblogspot.be\nblogspot.no\nblogspot.se\nblogspot.jp\nblogspot.in\nblogspot.ae\nblogspot.al\nblogspot.am\nblogspot.ba\nblogspot.bg\nblogspot.ch\nblogspot.cl\nblogspot.cz\nblogspot.dk\nblogspot.fi\nblogspot.gr\nblogspot.hk\nblogspot.hr\nblogspot.hu\nblogspot.ie\nblogspot.is\nblogspot.kr\nblogspot.li\nblogspot.lt\nblogspot.lu\nblogspot.md\nblogspot.mk\nblogspot.my\nblogspot.nl\nblogspot.no\nblogspot.pe\nblogspot.qa\nblogspot.ro\nblogspot.ru\nblogspot.se\nblogspot.sg\nblogspot.si\nblogspot.sk\nblogspot.sn\nblogspot.tw\nblogspot.ug\nblogspot.cat\n' >> $DOMAIN_FILE
-    echo 'Blogspot domains... Added.'
+    printf 'Blogspot domains... ' && _green 'Added\n'
 
     # Add twimg.edgesuite.net
     printf 'twimg.edgesuite.net\n' >> $DOMAIN_FILE
-    echo 'twimg.edgesuite.net... Added.'
+    printf 'twimg.edgesuite.net... ' && _green 'Added\n'
 
     # Add extra domains
     if [ ! -z $EXTRA_DOMAIN_FILE ]; then
         cat $EXTRA_DOMAIN_FILE >> $DOMAIN_FILE
     fi
-    echo 'Extra domain file '$EXTRA_DOMAIN_FILE'... Added.'
+    printf 'Extra domain file '$EXTRA_DOMAIN_FILE'... ' && _green 'Added\n'
 
     if [ $OUT_TYPE = 'DNSMASQ_RULES' ]; then
     # Convert domains into dnsmasq rules
         if [ $WITH_IPSET -eq 1 ]; then
-            echo 'Ipset rules included.'
+            _green 'Ipset rules included.'
             sort -u $DOMAIN_FILE | $SED_ERES 's#(.+)#server=/\1/'$DNS_IP'\#'$DNS_PORT'\
 ipset=/\1/'$IPSET_NAME'#g' > $CONF_TMP_FILE
         else
-            echo 'Ipset rules not included.'
+            _green 'Ipset rules not included.'
             sort -u $DOMAIN_FILE | $SED_ERES 's#(.+)#server=/\1/'$DNS_IP'\#'$DNS_PORT'#g' > $CONF_TMP_FILE
         fi
 
@@ -236,7 +253,7 @@ ipset=/\1/'$IPSET_NAME'#g' > $CONF_TMP_FILE
     fi
 
     cp $OUT_TMP_FILE $OUT_FILE
-    printf '\nConverting GfwList to '$OUT_TYPE'... Done.\n\n'
+    printf '\nConverting GfwList to '$OUT_TYPE'... ' && _green 'Done\n\n'
 
     # Clean up
     clean_and_exit 0
@@ -246,6 +263,7 @@ main() {
     if [ -z "$1" ]; then
         usage 0
     else
+        _green '\nJob Started.\n\n'
         check_depends
         get_args "$@"
         process
